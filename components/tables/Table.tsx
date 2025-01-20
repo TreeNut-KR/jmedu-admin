483a3ee00edd1d8746e831caec94f341c641a44e
bel1c10ud <mail@bellclou.de>
2025-01-20 18:17:39 +0900

    fix: 테이블이 비어있는 경우 스크롤바가 노출되는 문제 해결

--- components/tables/Table.tsx
+++ components/tables/Table.tsx
@@ -3,7 +3,7 @@ import { cn } from "@/utils/shadcn";
 
 const Table = React.forwardRef<HTMLTableElement, React.HTMLAttributes<HTMLTableElement>>(
   ({ className, ...props }, ref) => (
-    <div className="relative w-full overflow-auto">
+    <div className="relative w-full">
       <table
         ref={ref}
         className={cn("w-full caption-bottom divide-y border-b border-t text-sm", className)}
